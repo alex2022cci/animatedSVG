68cb4d56537c2d462de143f9ff07bad087ba635b
Alex2022CCI <alex2022cci@outlook.fr>
2022-12-16 09:19:32 +0100

    Update triangle.js
    more colors

--- triangle.js
+++ triangle.js
@@ -1,7 +1,7 @@
 const svg = document.getElementById('Frame1');
 
 svg.onclick = (e) => {
-    const colors = ['red','blue','green','orange','purple']
+    const colors = ['red','blue','green','orange','purple','darkgreen','darkorchid']
     const rando = () => colors[Math.floor(Math.random()*colors.length )];
     document.documentElement.style.cssText = `
     --dark-color:${rando()};
